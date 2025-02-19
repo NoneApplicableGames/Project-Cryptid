6a2ebf75d79ae6fd1ff235d2defcf97ff25f3504
NoneApplicableGames <158111174+NoneApplicableGames@users.noreply.github.com>
2025-02-19 17:06:42 +0000

    Finished Greyboxing

---
+++ Scripts/Character_Controller.gd
@@ -0,0 +1,9 @@
+extends CharacterBody3D
+
+@export_category("Gravity Properties")
+@export var weight : float
+
+@export_category("Speed Properties")
+@export var acceleration : float
+@export var max_speed : float
+@export var friction : float
