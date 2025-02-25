52f4227292cb729198625556750d807c485fc6fa
NoneApplicableGames <158111174+NoneApplicableGames@users.noreply.github.com>
2025-02-25 13:39:41 +0000

    Character Controller Statrted
    * Added light to playground scene
    * Added directional input to character

--- Scripts/Character_Controller.gd
+++ Scripts/Character_Controller.gd
@@ -5,5 +5,16 @@ extends CharacterBody3D
 
 @export_category("Speed Properties")
 @export var acceleration : float
-@export var max_speed : float
+@export var max_speed : float = 10
 @export var friction : float
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	#convert to vector 3 for velocity
+	
+	var direction := Vector3 (input_vector.x, 0.0, input_vector.y)
+	
+	print("Direction = ", direction)
+	
+	self.velocity = direction * max_speed
