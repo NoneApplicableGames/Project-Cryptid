e8a3655e1229d2944f13a79eb8c850dcbaecd053
NoneApplicableGames <158111174+NoneApplicableGames@users.noreply.github.com>
2025-04-14 17:07:46 +0100

    Working on jump physics

--- Scripts/Player_Character_Controller.gd
+++ Scripts/Player_Character_Controller.gd
@@ -8,17 +8,17 @@ class_name PlayerCharacterController
 
 ##Export properties for anything we need to tune the value for.
 
-@export_category("Movement Properties")
+@export_category("Ground Movement Properties")
 @export var max_speed : float = 10.0 ## Max speed the player can achive horrizontally.
 @export var acceleration : float ## The force applied to the player as they accelerate along the ground.
 @export var friction : float ## The force applied to the player as they come to a stop.
 
-@export_category("Jump Properties")
-@export var jump_force : float = 100.0 ##The force applied to the player when they initally jump.
 
-@export_category("Falling Properties")
-@export var mass : float = 1.0 ## The mass of the player character.
-const GRAVIY := 1.0 * Vector3.DOWN ##The force of gravity
+@export_category("Move Movement Properties")
+@export var jump_velocity : float = 10.0 ##The force applied to the player when they initally jump.
+@export var mass := 17.0 ##constant force of gravity applied to falling player
+@export var terminal_velocity := 20.0 ##player's max falling speed
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
@@ -40,11 +40,22 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
-	if is_on_floor() && Input.is_action_just_pressed("jump"):
-		self.velocity.y += jump_force
+	if Input.is_action_pressed("jump"):
+		var upwards_force := (jump_velocity * delta) * Vector3.UP
+		if is_on_floor():
+			# start by adding a fraction of the jump force into the 
+			self.velocity.y
 	
+	# gravity should only be applied if the player is falling. the player shouldnt have to fight
+	# gravity when jumping
 	if !is_on_floor():
-		self.velocity += GRAVIY * mass
+		#check if the jump button is being held before 
+		var down_force := (mass * gravity * delta) * Vector3.DOWN
+		self.velocity += down_force
+		# clamp fall speed so it doesnt go over terminal velocity
+		# we use maxf since velocity.y never needs a minimum value
+		self.velocity.y = maxf(velocity.y, -terminal_velocity)
 	
+	print ("Vertical Speed: " , velocity.y, "m/s")
 	move_and_slide()
 	
